445f18d47aa17e3c7379c5693924a4351900543e
dermotkel <G00364752@gmit.ie>
2019-12-23 00:05:07 +0000

    f

--- zgameDAO.py
+++ zgameDAO.py
@@ -1,12 +1,13 @@
 import mysql.connector
+import dbconfig as cfg
 class gameDAO:
     db=""
     def __init__(self): 
         self.db = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="",
-        database="datarepresentation"
+        host=cfg.mysql['host'],
+        user=cfg.mysql['user'],
+        password=cfg.mysql['password'],
+        database=cfg.mysql['database']
         )
     
             
